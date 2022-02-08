0ca18c75e04d7dda715831f9295fbaad0f81eec4
Jaitank <tankjai123@gmail.com>
2022-02-08 06:45:42 +0530

    JavaScript first program

---
+++ 01_first_code.js
@@ -0,0 +1,22 @@
+// Lets create a user's information with variables:
+
+var fullName =  "Jai Tank";
+
+const uid = "abc123";   // we can't overwrite it becuase this is not variable, and we want it as unique id
+
+var email = "tankjai23@gmail.com";
+
+var password = "123456";
+
+var country = "India";
+
+var state = "Rajasthan";
+
+
+console.log(`
+          With User ID: ${uid}
+          User is : ${fullName}
+          having email : ${email}
+          from : ${state} , ${country}
+          is registered.
+`);
